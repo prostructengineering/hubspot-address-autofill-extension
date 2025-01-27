ca17c148ddbca024e8e6916112625f90ed083497
Abhijay Rana <abhijayrana@gmail.com>
2025-01-27 09:58:39 -0800

    onrender

--- extension/content.js
+++ extension/content.js
@@ -47,7 +47,9 @@ window.onload = function () {
     console.log("Hidden input focused");
     if (!window.google) {
       console.log("Loading Google Maps API");
-      fetch("https://your-app.onrender.com/api/maps-key")
+      fetch(
+        "https://hubspot-address-autofill-extension.onrender.com/api/maps-key"
+      )
         .then((response) => response.json())
         .then((data) => {
           const script = document.createElement("script");
